c68060631ca741e71033cf98ac0bea4dbdff2a5b
znl <znl@Polaris>
2024-04-12 22:37:55 +0800

    check1-1

--- src/reassembler.cc
+++ src/reassembler.cc
@@ -4,14 +4,56 @@ using namespace std;
 
 void Reassembler::insert( uint64_t first_index, string data, bool is_last_substring )
 {
-  // Your code here.
-  (void)first_index;
-  (void)data;
-  (void)is_last_substring;
+  //重组数据
+  if(first_index>=unassembled_idx && first_index-unassembled_idx<capacity)
+  {
+    auto psize = min(data.size(),
+    unassembled_idx+capacity-first_index);
+
+    auto start = std::next(buffer.begin(),first_index-unassembled_idx);
+    std::copy_n(data.begin(),psize,start);
+    
+    auto mask_start = std::next(valid_mask.begin(),first_index-unassembled_idx);
+    std::fill_n(mask_start,psize,true);
+  }
+
+  //尝试output
+  auto mask_iter = valid_mask.begin();
+  auto& writer_ref = static_cast<Writer&>(output_);
+  for(;mask_iter!=valid_mask.end()&&*mask_iter==true;++mask_iter);
+  
+  if(mask_iter != valid_mask.begin())
+  {
+    auto output_end = std::next(buffer.begin(),(mask_iter-valid_mask.begin()));
+    writer_ref.push(std::string(buffer.begin(),output_end));
+    unassembled_idx = writer_ref.bytes_pushed();
+
+    //输出完成后移动unassembled的数据
+    std::copy(output_end,buffer.end(),buffer.begin());
+    auto invalid_iter = std::copy(mask_iter,valid_mask.end(),valid_mask.begin());
+    std::fill(invalid_iter,valid_mask.end(),false);
+  }
+
+  if(is_last_substring)
+    writer_ref.close();
 }
 
 uint64_t Reassembler::bytes_pending() const
 {
   // Your code here.
-  return {};
+  uint64_t pendings = 0;
+  for(const auto b : valid_mask)
+  {
+    if(b)
+      ++pendings;
+  }
+  return pendings;
+}
+
+
+Reassembler::Reassembler( ByteStream&& output ): 
+output_( std::move( output ) ),capacity(writer().available_capacity()),
+buffer(capacity) , valid_mask(capacity,false) 
+{
+
 }
--- src/reassembler.hh
+++ src/reassembler.hh
@@ -1,12 +1,14 @@
 #pragma once
 
 #include "byte_stream.hh"
+#include <vector>
+#include <algorithm>
 
 class Reassembler
 {
 public:
   // Construct Reassembler to write into given ByteStream.
-  explicit Reassembler( ByteStream&& output ) : output_( std::move( output ) ) {}
+  explicit Reassembler( ByteStream&& output );
 
   /*
    * Insert a new substring to be reassembled into a ByteStream.
@@ -42,4 +44,8 @@ public:
 
 private:
   ByteStream output_; // the Reassembler writes to this ByteStream
+  size_t capacity {};
+  std::vector<char> buffer;
+  std::vector<bool> valid_mask;
+  size_t unassembled_idx {};
 };
