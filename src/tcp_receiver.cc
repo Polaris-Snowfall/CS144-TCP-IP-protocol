08f102a8366c6d3364c4edf49e1e3db596b804b7
znl <811614792@qq.com>
2024-04-13 22:40:07 +0800

    check2.2

--- src/tcp_receiver.cc
+++ src/tcp_receiver.cc
@@ -4,12 +4,31 @@ using namespace std;
 
 void TCPReceiver::receive( TCPSenderMessage message )
 {
-  // Your code here.
-  (void)message;
+  if(message.SYN)
+    ISN = message.seqno;
+  
+  if(ISN.has_value())
+  {
+    uint64_t first_index = message.seqno.unwrap(*ISN,writer().bytes_pushed())+message.SYN-1;
+    reassembler_.insert(first_index,message.payload,message.FIN);
+  }
+
+  if(message.RST)
+  {
+    reader().set_error();
+  }
+  
 }
 
 TCPReceiverMessage TCPReceiver::send() const
 {
-  // Your code here.
-  return {};
+  TCPReceiverMessage message {};
+  if(ISN.has_value())
+  {
+    message.ackno = Wrap32::wrap(writer().bytes_pushed()+1+writer().is_closed(),*ISN);
+  }
+  message.window_size = min(writer().available_capacity(),static_cast<uint64_t>(UINT16_MAX));
+
+  message.RST = writer().has_error(); 
+  return message;
 }
