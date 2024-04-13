5be34862ea6793ad10e260d440bcf08c2f025c54
znl <811614792@qq.com>
2024-04-13 19:11:32 +0800

    check2.1

--- src/wrapping_integers.cc
+++ src/wrapping_integers.cc
@@ -4,16 +4,18 @@ using namespace std;
 
 Wrap32 Wrap32::wrap( uint64_t n, Wrap32 zero_point )
 {
-  // Your code here.
-  (void)n;
-  (void)zero_point;
-  return Wrap32 { 0 };
+  return Wrap32 { zero_point+n };
 }
 
 uint64_t Wrap32::unwrap( Wrap32 zero_point, uint64_t checkpoint ) const
 {
-  // Your code here.
-  (void)zero_point;
-  (void)checkpoint;
-  return {};
+  uint32_t ckmod = wrap(checkpoint,zero_point).raw_value_;
+  uint32_t off = raw_value_-ckmod;
+  uint64_t upper = static_cast<uint64_t>(UINT32_MAX)+1;
+  
+  if(off <= (upper>>1) || checkpoint+off<upper)
+    return static_cast<uint64_t>(checkpoint+off);
+
+  return static_cast<uint64_t>(checkpoint+off-upper);
+
 }
